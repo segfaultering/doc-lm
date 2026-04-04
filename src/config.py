80246f145a258e8a240f9783ff9ce0c116a91984
segfaultering <261435586+segfaultering@users.noreply.github.com>
2026-04-04 11:48:34 +0500

    Fixed syntax errors in previous commit.

--- src/config.py
+++ src/config.py
@@ -9,15 +9,32 @@ import os
 LLM = None
 EMBEDDING_MODEL = None
 VEC_DB = None
+SQL_DB_CONN = None
+
+BUILD_SCHEMA_QUERIES: list[str] = [
+    """
+    CREATE TABLE pdf(
+        id INT PRIMARY KEY,
+        title TEXT
+    );
+    """, 
+
+    """
+    CREATE TABLE pdf_splits(
+        id INT PRIMARY KEY,
+        pdf_id INT FOREIGN KEY
+    )
+    """
+]
 
 class Paths(Enum):
-    DATA: Path = Path(r"../data/raw/")
-    SQL_DB: Path = Path(r"../data/database.db")
-    VEC_DB: Path = Path(r"../data/vec/")
+    DATA = Path(r"../data/raw/")
+    SQL_DB = Path(r"../data/database.db")
+    VEC_DB = Path(r"../data/vec/")
 
 class ApiKeys(StrEnum):
-    GROQ: str = os.environ(GROQ_API_KEY)   
-    GOOGLE_GENAI: str = os.environ(GOOGLE_GENAI_KEY)
+    GROQ = os.environ["GROQ_API_KEY"]
+    GOOGLE_GENAI = os.environ["GOOGLE_GENAI_KEY"]
     
 class SqlQueries(StrEnum):
     pass
--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,43 @@
 import config as cfg
 
+import sqlite3
+
+from langchain_groq import ChatGroq
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
 
 
+def _init_llm():
+    return ChatGroq(
+        model="qwen/qwen3.5-32b",
+        api_key=cfg.ApiKeys.GROQ.value
+    )
 
+def _init_embedding_model():
+    return GoogleGenerativeAIEmbeddings(
+        model="gemini-embedding-001",
+        google_api_key=cfg.ApiKeys.GOOGLE_GENAI.value
+    )
+        
+def _init_vec_db():
+    return Chroma(
+        embedding_function=cfg.EMBEDDING_MODEL,
+        persist_directory=cfg.Paths.VEC_DB.value
+    )
 
+def _init_sql_db():
+    cfg.SQL_DB_CONN = sqlite3.connect(cfg.Paths.SQL_DB.value)    
+    with cfg.SQL_DB_CONN as conn:
+        for query in cfg.BUILD_SCHEMA_QUERIES:
+            conn.execute(query)
 
+    cfg.SQL_DB_CONN.commit()
 
+def setup() -> None:
+    cfg.LLM = _init_llm()
+    cfg.EMBEDDING_MODEL = _init_embedding_model()
+    cfg.SQL_DB_CONN = _init_sql_db()
+    cfg.VEC_DB = _init_vec_db()
 
-def setup():
-    pass
-
+if __name__ == "__main__":
+    setup()
